958ee2f3ab73fd54b1c8bdb8bddc1d8e9013663f
my lovely puppy <98075420+loulou-lee@users.noreply.github.com>
2022-09-12 23:16:14 +0900

    ruby 1.9.2 부터 liquid URI escape deprecated따른 CGI escape로 수정
    ruby 1.9.2 부터 liquid URI escape deprecated되어서 CGI escape 방식 사용

--- _plugins/url_encode.rb
+++ _plugins/url_encode.rb
@@ -1,12 +1,15 @@
 # _plugins/url_encode.rb
 require 'liquid'
-require 'uri'
+# require 'uri'
+require 'cgi'
 
 # Percent encoding for URI conforming to RFC 3986.
 # Ref: http://tools.ietf.org/html/rfc3986#page-12
 module URLEncode
   def url_encode(url)
-    return URI.escape(url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+    # ruby 1.9.2 부터 liquid URI escape deprecated되어서 CGI escape 방식 사용
+    # return URI.escape(url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+    return CGI::escape(url)
   end
 end
 
